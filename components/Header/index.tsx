f6125d461be0b5e73bb3facfac0c214ab5678ec5
Miguel Ferreira <miguel.ferreira.2003@outlook.com>
2022-03-14 18:26:12 +0000

    update: commit issues

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head';
 export default function Header(title?: any) : any {
     return (
         <Head>
-            <title>CMFlores {(title.title) ? "| " + title.title : ""}</title>
+            <title>Avogg {(title.title) ? "| " + title.title : ""}</title>
             <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Roboto&display=swap" rel="stylesheet"></link>
         </Head>
     );
